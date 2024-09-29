4235c49ad62efb4f0e2c50209d5bceccfa4c634c
Nivas-art <srinujunnu123@gmail.com>
2024-09-29 21:37:48 +0530

    some

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.63.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-vpc-bktt"
+    key    = "expense-aws-sg-alb"
+    region = "us-east-1"
+    dynamodb_table = "terraform-vpc-tablee"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
